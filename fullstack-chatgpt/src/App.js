4e104ce3d2d9fd967e7f6f9b00c33703bbde810b
BOBCATSON <robjcatlin@gmail.com>
2023-02-10 00:47:19 +0000

    added /post route, added axios, cors

--- fullstack-chatgpt/src/App.js
+++ fullstack-chatgpt/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001", { username, password })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Let the games begin</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type='text' value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type='password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const uri =
   "mongodb+srv://robcatlin:dyb55nsFpcBdE12kMY5q9z4FEpXvNE@cluster0.jo9w9oa.mongodb.net/?retryWrites=true&w=majority";
@@ -33,11 +37,26 @@ const user = new User({
 
 user.save((error) => {
   if (error) throw error;
-  console.log("New user has been added to the database");
+  console.log("User added to the database");
 });
 
 app.get("/", (req, res) => {
   res.send("Server is up and running dawg!");
+});
+
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password,
+  });
+  user.save((error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send("User saved to the database");
+    }
+  });
 });
 
 const port = process.env.PORT || 3001;
